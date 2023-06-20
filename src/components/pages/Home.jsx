ae617d071c180dd2858906a64d7a25e644574d3d
jeremyberg1 <128089852+jeremyberg1@users.noreply.github.com>
2023-06-20 12:46:08 -0400

    Homepage
    Added Grid/Cards for Search

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import { Box, Card, CardContent, Grid } from "@mui/material";
+import React from "react";
+import parks from "../../assets/parks.png";
+import NationalParksSearch from "../NationalParksSearch";
+import MountainsSearch from "../MountainsSearch";
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <Box sx={{ width: "100vw", height: "100vh", margin: "0" }}>
+      <Box sx={{}}>
+        <img src={parks} alt="National Park" />
+      </Box>
+      <Grid>
+        <Grid item>
+          <Card>
+            <CardContent>
+              <NationalParksSearch />
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item>
+          <Card>
+            <CardContent>
+              <MountainsSearch />
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
