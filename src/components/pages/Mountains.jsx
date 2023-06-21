09f117891cd4b883eb65aef55263671f634097b1
Kevin Carter <kj.carter.design@gmail.com>
2023-06-21 17:08:48 -0400

    add search bar to pages

--- src/components/pages/Mountains.jsx
+++ src/components/pages/Mountains.jsx
@@ -1,11 +1,12 @@
 import { Box } from '@mui/material'
 import React from 'react'
+import SearchBar from '../SearchBar'
 
 export default function Mountains() {
   return (
-    <Box sx={{width:"100vw",Height:"100vh",backgroundColor:"blue"}}
+    <Box sx={{width:"100vw",Height:"100vh"}}
     >
-      <p>Test</p>
+      <SearchBar />
     </Box>
   )
 }
